196f92b5745685831c1188819c29893f3e5d8e39
Warren R. Bank <warren.r.bank@gmail.com>
2017-07-20 16:09:12 -0700

    new option causes return value to be hash that includes both "txData" and "signature"

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 const rlp = require('./lib/rlp')  // {decode, toBuffer, isHexPrefixed, stripHexPrefix, padToEven, intToBuffer, safeParseInt, bufferToHex}
 
-const fields = ["nonce","gasPrice","gasLimit","to","value","data"]
+const field_sets = {
+  params:    ["nonce","gasPrice","gasLimit","to","value","data"],
+  signature: ["v","r","s"]
+}
 
-const format_fields = function(rawData, to_hex, add_prefix) {
+const format_fields = function(fields, rawData, to_hex, add_prefix) {
   let txData = {}
 
   fields.forEach((field, index) => {
@@ -19,7 +22,7 @@ const format_fields = function(rawData, to_hex, add_prefix) {
 // ===
 // API
 // ===
-const unsign = function(rawTx, to_hex=true, add_prefix=true) {
+const unsign = function(rawTx, to_hex=true, add_prefix=true, include_signature=false) {
   let rawData
   if (typeof rawTx === 'string') {
     rawTx = Buffer.from(rlp.stripHexPrefix(rawTx), 'hex')
@@ -28,16 +31,28 @@ const unsign = function(rawTx, to_hex=true, add_prefix=true) {
     rawData = rlp.decode(rawTx)
   }
 
-  if (rawData === undefined)          throw new Error('TypeError: raw transaction must be either a Buffer or hex-encoded String value')
-  if (! Array.isArray(rawData))       throw new Error('TypeError: raw transaction is not RLP encoded')
-  if (rawData.length < fields.length) throw new Error('FormatError: RLP encoded raw transaction contains too few data fields')
+  if (rawData === undefined)
+    throw new Error('TypeError: raw transaction must be either a Buffer or hex-encoded String value')
+  if (! Array.isArray(rawData))
+    throw new Error('TypeError: raw transaction is not RLP encoded')
+  if (rawData.length < field_sets.params.length)
+    throw new Error('FormatError: RLP encoded raw transaction contains too few data fields')
+  if (include_signature && (rawData.length < (field_sets.params.length + field_sets.signature.length)))
+    throw new Error('FormatError: RLP encoded raw transaction contains too few data fields')
 
   for (let i=0; i<rawData.length; i++) {
     rawData[i] = rlp.toBuffer(rawData[i])
   }
 
-  let txData  = format_fields(rawData, to_hex, add_prefix)
-  return txData
+  let txData = format_fields(field_sets.params, rawData.slice(0, field_sets.params.length), to_hex, add_prefix)
+
+  if (include_signature) {
+    let signature = format_fields(field_sets.signature, rawData.slice(field_sets.params.length), to_hex, add_prefix)
+    return {txData, signature}
+  }
+  else {
+    return txData
+  }
 }
 
 module.exports = unsign
